993b51e926ee3724bcb2b4cf984f0d4219908960
Juan Bautista Malina <malinajuanbautista@gmail.com>
2024-10-31 17:23:39 -0300

    Estilos a los links

--- src/assets/components/SideNav.tsx
+++ src/assets/components/SideNav.tsx
@@ -1,4 +1,4 @@
-import { Heading } from "@chakra-ui/react";
+import { Heading, Link, VStack } from "@chakra-ui/react";
 import { Category } from "../../types";
 
 type Props = {
@@ -6,13 +6,36 @@ type Props = {
   loading: boolean;
 };
 
-function SideNav({}: Props) {
+const selectedProps = {
+  bgColor: "blue.400",
+  color: "white",
+  fontWeight: "bold"
+}
+
+function SideNav({ categories }: Props) {
+  
+  const selected = {
+    strCategory: "Beef",
+  }
+
+
   return (
     <>
       <Heading color="blue.400" fontSize={12} fontWeight="bold" mb={4}>
         Categorías
       </Heading>
-      
+      <VStack align="stretch">
+        {categories.map((c) => (
+          <Link
+          px={2}
+          py={1}
+          borderRadius={5}
+          key={c.strCategory}
+          _hover={{ textDecoration: "none" }}
+          {...(selected.strCategory == c.strCategory && selectedProps )}
+          >{c.strCategory}</Link>
+        ))}
+      </VStack>
     </>
   );
 }
