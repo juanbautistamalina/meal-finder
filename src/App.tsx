7af363bbe5a54f37a290ddf4b59d58b516207add
Juan Bautista Malina <malinajuanbautista@gmail.com>
2024-11-09 07:38:18 -0300

    Mostrando Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Grid, GridItem, useDisclosure } from "@chakra-ui/react";
 import MainContent from "./assets/components/MainContent";
 import SideNav from "./assets/components/SideNav";
 import Header from "./assets/components/Header";
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { Category, Meal, SearchForm } from "./types";
 import useHttpData from "./hooks/useHttpData";
 import axios from "axios";
+import RecipeModal from "./assets/components/RecipeModal";
 
 const url = "https://www.themealdb.com/api/json/v1/1/list.php?c=list";
 const makeMealUrl = (category: Category) =>
@@ -16,6 +17,7 @@ const defaultCategory = {
 };
 
 function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [selectedCategory, setSelectedCategory] =
     useState<Category>(defaultCategory);
 
@@ -28,54 +30,56 @@ function App() {
   } = useHttpData<Meal>(makeMealUrl(defaultCategory));
 
   const searchApi = (searchForm: SearchForm) => {
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchForm.search}`
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchForm.search}`;
     setLoadingMeal(true);
     axios
-      .get<{meals: Meal[]}>(url)
-      .then(({data}) => setMeals(data.meals))
-      .finally(() => setLoadingMeal(false))
-
+      .get<{ meals: Meal[] }>(url)
+      .then(({ data }) => setMeals(data.meals))
+      .finally(() => setLoadingMeal(false));
   };
 
   return (
-    <Grid
-      templateAreas={`"header header"
+    <>
+      <Grid
+        templateAreas={`"header header"
                   "nav main"`}
-      gridTemplateRows={"60px 1fr"}
-      gridTemplateColumns={{ sm: `0 1fr`, md: `250px 1fr` }}
-      fontSize={14}
-    >
-      <GridItem
-        boxShadow="lg"
-        zIndex="1"
-        pos="sticky"
-        top="0"
-        pt="7px"
-        bg="white"
-        area={"header"}
+        gridTemplateRows={"60px 1fr"}
+        gridTemplateColumns={{ sm: `0 1fr`, md: `250px 1fr` }}
+        fontSize={14}
       >
-        <Header onSubmit={searchApi} />
-      </GridItem>
-      <GridItem
-        pos="sticky"
-        top="60px"
-        left="0"
-        p="5"
-        area={"nav"}
-        height="calc(100vh - 60px)"
-        overflowY="auto"
-      >
-        <SideNav
-          categories={data}
-          loading={loading}
-          selectedCategory={selectedCategory}
-          setSelectedCategory={setSelectedCategory}
-        />
-      </GridItem>
-      <GridItem p="4" bg="gray.100" area={"main"}>
-        <MainContent loading={loadingMeal} meals={dataMeal} />
-      </GridItem>
-    </Grid>
+        <GridItem
+          boxShadow="lg"
+          zIndex="1"
+          pos="sticky"
+          top="0"
+          pt="7px"
+          bg="white"
+          area={"header"}
+        >
+          <Header onSubmit={searchApi} />
+        </GridItem>
+        <GridItem
+          pos="sticky"
+          top="60px"
+          left="0"
+          p="5"
+          area={"nav"}
+          height="calc(100vh - 60px)"
+          overflowY="auto"
+        >
+          <SideNav
+            categories={data}
+            loading={loading}
+            selectedCategory={selectedCategory}
+            setSelectedCategory={setSelectedCategory}
+          />
+        </GridItem>
+        <GridItem p="4" bg="gray.100" area={"main"}>
+          <MainContent openRecipe={onOpen} loading={loadingMeal} meals={dataMeal} />
+        </GridItem>
+      </Grid>
+      <RecipeModal isOpen={isOpen} onClose={onClose} />
+    </>
   );
 }
 
--- src/assets/components/RecipeModal.tsx
+++ src/assets/components/RecipeModal.tsx
@@ -1,9 +1,33 @@
-type Props = {}
+import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from "@chakra-ui/react";
 
-function RecipeModal({}: Props) {
-  return (
-    <div>RecipeModal</div>
-  )
-}
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+};
 
-export default RecipeModal
+function RecipeModal({isOpen, onClose}: Props) {
+   
+    return (
+      <>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              Receta
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={onClose}>
+                Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    );
+  };
+
+
+export default RecipeModal;
