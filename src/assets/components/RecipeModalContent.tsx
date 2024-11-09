eabdca6db09cf97f6aa4b0beca8b2f1f0555059d
Juan Bautista Malina <malinajuanbautista@gmail.com>
2024-11-09 14:27:54 -0300

    Estilo de las recetas

--- src/assets/components/RecipeModalContent.tsx
+++ src/assets/components/RecipeModalContent.tsx
@@ -1,3 +1,4 @@
+import { ModalBody, ModalCloseButton, ModalHeader, Image, Heading, Text } from "@chakra-ui/react"
 import { MealDetails } from "../../types"
 
 type Props = {
@@ -6,7 +7,15 @@ type Props = {
 
 function RecipeModalContent({data}: Props) {
   return (
-    <div>RecipeModalContent</div>
+    <>
+        <ModalHeader>{data.strMeal}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+            <Image width="100%" borderRadius="lg" src={data.strMealThumb} alt={data.strMeal}/>
+            <Heading mt="4" mb="4" size="md">Ingredientes</Heading>
+            <Text whiteSpace="pre-line" mt="4">{data.strInstructions}</Text>
+        </ModalBody>
+    </>
   )
 }
 
